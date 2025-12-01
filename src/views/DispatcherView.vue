32e001ecfb2450127443c6a7809860b56dfc08ba
Eddie Krantz <eddiekrantz@MacBook-Air-som-tillhor-Eddie.local>
2025-12-01 14:45:22 +0100

    g

--- src/views/DispatcherView.vue
+++ src/views/DispatcherView.vue
@@ -1,73 +1,109 @@
 <template>
-    <div id="orders">
-      <div id="orderList">
-        <div v-for="(order, key) in orders" v-bind:key="'order'+key">
-          #{{ key }}: {{ order.orderItems.join(", ") }}
-        </div>
-        <button v-on:click="clearQueue">Clear Queue</button>
+  <div id="dispatcher">
+
+    <div id="orderList">
+      <div v-for="(order, key) in orders" :key="key">
+        <strong>#{{ key }}</strong><br>
+        {{ order.orderItems.join(", ") }}<br>
+        {{ order.customer.fullname }}
       </div>
-      <div id="dots">
-          <div v-for="(order, key) in orders" v-bind:style="{ left: order.details.x + 'px', top: order.details.y + 'px'}" v-bind:key="'dots' + key">
-            {{ key }}
-          </div>
+
+      <button @click="clearQueue">Clear Queue</button>
+    </div>
+
+    <div id="map-area">
+      <img id="dispatch-map" src="/img/polacks.jpg">
+
+      <div
+        v-for="(order, key) in orders"
+        :key="'dot' + key"
+        class="dot"
+        :style="{ 
+          left:  (order.details.xPercent * 1920) + 'px',
+          top:   (order.details.yPercent * 1078) + 'px'
+        }"
+      >
+        {{ key }}
       </div>
     </div>
-  </template>
-  <script>
-  import io from 'socket.io-client'
-  const socket = io("localhost:3000");
-  
-  export default {
-    name: 'DispatcherView',
-    data: function () {
-      return {
-        orders: null,
-      }
-    },
-    created: function () {
-      socket.on('currentQueue', data =>
-        this.orders = data.orders);
-    },
-    methods: {
-      clearQueue: function () {
-        socket.emit('clearQueue');
-      },
-      changeStatus: function(orderId) {
-        socket.emit('changeStatus', {orderId: orderId, status: "Annan status"});
 
-      }
+  </div>
+</template>
+
+<script>
+import io from "socket.io-client"
+const socket = io("http://localhost:3000")
+
+export default {
+  name: "DispatcherView",
+
+  data() {
+    return {
+      orders: {}
+    }
+  },
+
+  created() {
+    socket.on("currentQueue", (data) => {
+      this.orders = data.orders
+    })
+
+    socket.on("orderAdded", (data) => {
+      this.orders[data.orderId] = data
+    })
+
+    socket.on("queueCleared", () => {
+      this.orders = {}
+    })
+  },
+
+  methods: {
+    clearQueue() {
+      socket.emit("clearQueue")
     }
   }
-  </script>
-  <style>
-  #orderList {
-    top:1em;
-    left:1em;
-    position: absolute;
-    z-index: 2;
-    color:black;
-    background: rgba(255,255,255, 0.5);
-    padding: 1em;
-  }
-  #dots {
-    position: relative;
-    margin: 0;
-    padding: 0;
-    background-repeat: no-repeat;
-    width:1920px;
-    height: 1078px;
-    cursor: crosshair;
-    background-image: url('/img/polacks.jpg');
-  }
-  
-  #dots div {
-    position: absolute;
-    background: black;
-    color: white;
-    border-radius: 10px;
-    width:20px;
-    height:20px;
-    text-align: center;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+#dispatcher {
+  position: relative;
+}
+
+#orderList {
+  position: absolute;
+  top: 1em;
+  left: 1em;
+  z-index: 10;
+  background: rgba(255,255,255,0.9);
+  padding: 1em;
+  border-radius: 6px;
+  font-size: 14px;
+}
+
+#map-area {
+  position: relative;
+  width: 1920px;
+  height: 1078px;
+  margin: 0 auto;
+}
+
+#dispatch-map {
+  width: 1920px;
+  height: 1078px;
+  display: block;
+}
+
+.dot {
+  position: absolute;
+  width: 22px;
+  height: 22px;
+  background: black;
+  color: white;
+  border-radius: 50%;
+  text-align: center;
+  line-height: 22px;
+  font-size: 12px;
+  transform: translate(-50%, -50%);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,58 +1,201 @@
 <template>
-<div>
-    <div>
-    <h1>Burgers</h1>
-    <Burger v-for="burger in burgers"
-            v-bind:burger="burger" 
-            v-bind:key="burger.name"/>
-    </div>
-    <div id="map" v-on:click="addOrder">
-    click here
-    </div>
-</div>
+  <div>
+
+    <header>
+      <span>Welcome to Burgirburgir Online</span>
+    </header>
+
+    <main>
+
+      <section id="burger-section">
+        <h2>Select burger</h2>
+        <p>This is where you execute burger selection</p>
+
+        <div 
+          class="burger-item"
+          v-for="(burger, index) in burgers"
+          :key="index"
+        >
+          <h3>{{ burger.name }}</h3>
+          <img :src="burger.img" :alt="burger.name">
+          <ul>
+            <li>{{ burger.kCal }} kCal</li>
+            <li v-if="burger.lactose">Contains lactose</li>
+            <li v-if="burger.gluten">Contains gluten</li>
+          </ul>
+        </div>
+      </section>
+
+      <section id="customer-section">
+        <h2>Customer information</h2>
+
+        <form>
+
+          <p>
+            <label>Full name</label>
+            <input type="text" v-model="customer.fullname">
+          </p>
+
+          <p>
+            <label>Email</label>
+            <input type="email" v-model="customer.email">
+          </p>
+
+          <p>
+            <label>Street</label>
+            <input type="text" v-model="customer.street">
+          </p>
+
+          <p>
+            <label>House</label>
+            <input type="number" v-model="customer.house">
+          </p>
+
+          <p>
+            <label>Payment options</label>
+            <select v-model="customer.payment">
+              <option>Credit card</option>
+              <option>Swish</option>
+              <option>PayPal</option>
+              <option>Klarna</option>
+            </select>
+          </p>
+
+          <p>
+            <label>Gender</label>
+            <label><input type="radio" value="male" v-model="customer.gender"> Male</label>
+            <label><input type="radio" value="female" v-model="customer.gender"> Female</label>
+            <label><input type="radio" value="nonbinary" v-model="customer.gender"> Non-binary</label>
+            <label><input type="radio" value="undisclosed" v-model="customer.gender"> Undisclosed</label>
+          </p>
+
+        </form>
+      </section>
+
+      <section id="map-section">
+        <h2>Select delivery location</h2>
+
+        <div id="map-container">
+          <div id="map-wrapper">
+            <img id="map-image" src="/img/polacks.jpg" @click="setLocation">
+
+            <div v-if="location"
+                id="marker"
+                :style="{ 
+                  left: location.x + 'px', 
+                  top: location.y + 'px' 
+                }">
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </main>
+
+    <button @click="placeOrder">
+      <img src="https://cdn-icons-png.flaticon.com/512/4297/4297679.png"
+           style="width: 20px; filter: invert(1); margin-right: 6px;">
+      Place my order!
+    </button>
+
+    <footer>© 2025 Burgirburgir</footer>
+
+  </div>
 </template>
 
 <script>
-import Burger from '../components/OneBurger.vue'
-import io from 'socket.io-client'
+import io from "socket.io-client"
+const socket = io("http://localhost:3000")
 
-const socket = io("localhost:3000");
+function MenuItem(name, kCal, lactose, gluten, img) {
+  this.name = name
+  this.kCal = kCal
+  this.lactose = lactose
+  this.gluten = gluten
+  this.img = img
+}
 
 export default {
-  name: 'HomeView',
-  components: {
-    Burger
-  },
-  data: function () {
+  data() {
     return {
-      burgers: [ {name: "small burger", kCal: 250},
-                 {name: "standard burger", kCal: 450},
-                 {name: "large burger", kCal: 850}
-               ]
+      location: null,
+
+      customer: {
+        fullname: "",
+        email: "",
+        street: "",
+        house: "",
+        payment: "Credit card",
+        gender: "male"
+      },
+
+      burgers: [
+        new MenuItem("The Fire Burger", 750, true, true, "/img/fire-burger.jpg"),
+        new MenuItem("Fried Turkey Burger", 600, true, true, "/img/turkey-burger.jpg"),
+        new MenuItem("A Double w/ Cheese", 1800, true, true, "/img/double-cheese.jpg")
+      ]
     }
   },
+
   methods: {
-    getOrderNumber: function () {
-      return Math.floor(Math.random()*100000);
+    setLocation(event) {
+      const rect = event.target.getBoundingClientRect()
+
+      const clickX = event.clientX - rect.left
+      const clickY = event.clientY - rect.top
+
+      this.location = {
+        x: Math.round(clickX),
+        y: Math.round(clickY),
+        xPercent: clickX / rect.width,
+        yPercent: clickY / rect.height
+      }
     },
-    addOrder: function (event) {
-      var offset = {x: event.currentTarget.getBoundingClientRect().left,
-                    y: event.currentTarget.getBoundingClientRect().top};
-      socket.emit("addOrder", { orderId: this.getOrderNumber(),
-                                details: { x: event.clientX - 10 - offset.x,
-                                           y: event.clientY - 10 - offset.y },
-                                orderItems: ["Beans", "Curry"]
-                              }
-                 );
+
+    placeOrder() {
+      if (!this.location) {
+        alert("Click the map to choose a delivery location!")
+        return
+      }
+
+      socket.emit("addOrder", {
+        orderItems: this.burgers.map(b => b.name),
+        customer: this.customer,
+        details: {
+          xPercent: this.location.xPercent,
+          yPercent: this.location.yPercent
+        }
+      })
+
+      alert("Order placed!")
     }
   }
 }
 </script>
 
-<style>
-  #map {
-    width: 300px;
-    height: 300px;
-    background-color: red;
-  }
+<style scoped>
+#map-wrapper {
+  position: relative;
+  width: 100%;
+  aspect-ratio: 1920 / 1078;
+  overflow: hidden;
+  border: 2px solid #ccc;
+}
+
+#map-image {
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+  cursor: crosshair;
+}
+
+#marker {
+  position: absolute;
+  width: 20px;
+  height: 20px;
+  background: blue;
+  border-radius: 50%;
+  z-index: 10;
+  transform: translate(-50%, -50%);
+}
 </style>
